cab20bbd47953f304619211ec521db12cd5774ec
Angelo <angelomaiale@gmail.com>
2026-01-29 18:25:51 -0400

    Feat: remove, prior, next, etc

---
+++ node.h
@@ -0,0 +1,17 @@
+template<class T>
+class node {
+private:
+    T data;
+    node<T>* next = nullptr;
+    node<T>* prev = nullptr;
+public:
+    node(T data, node<T>* next = nullptr, node<T>* prev = nullptr) : data(data), next(next), prev(prev) {}
+    ~node() {}
+    T getData() { return data; }
+    void setData(T d) { data = d; }
+    node<T>* getNext() { return next; }
+    void setNext(node<T>* n) { next = n; }
+    node<T>* getPrev() { return prev; }
+    void setPrev(node<T>* p) { prev = p; }
+    void print() { cout << data << " "; }
+};
